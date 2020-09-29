6c2201a23d5c672f1d6d84c1a88f2c99b9f2bb45
Ali Furkan GÖKÇE <ali.gokce@std.izu.edu.tr>
2020-09-29 12:44:02 +0300

    AddRoles

---
+++ DataAccess/Concrete/EntityFramework/EfUserDal.cs
@@ -0,0 +1,29 @@
+﻿using Core.DataAccess.EntityFramework;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using Core.Entities.Concrete;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfUserDal : EfEntityRepositoryBase<User, NorthwindContext>, IUserDal
+    {
+        public List<OperationClaim> GetClaims(User user)
+        {
+            using (var context=new NorthwindContext())
+            {
+                var result = from operationclaim in context.OperationClaims
+                             join userOperationClaim in context.UserOperationClaims
+                             on operationclaim.Id equals userOperationClaim.OperationClaimId
+                             where userOperationClaim.UserId == user.Id
+                             select new OperationClaim { Id = operationclaim.Id, Name = operationclaim.Name };
+
+                return result.ToList();          
+
+            }
+        }
+    }
+}
