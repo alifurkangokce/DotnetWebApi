a426f6163bc24652741f9c82872988461bb5cf93
Ali Furkan GÖKÇE <ali.gokce@std.izu.edu.tr>
2020-09-29 16:25:33 +0300

    Jwt İmplementasion

--- Business/Contants/Messages.cs
+++ Business/Contants/Messages.cs
@@ -9,5 +9,11 @@ namespace Business.Contants
         public static string ProductAdded = "Ürün başarıyla eklendi";
         public static string ProductDeleted = "Ürün başarıyla silindi";
         public static string ProductUpdated = "Ürün başarıyla güncellendi";
+        public static string UserNotFound = "Kullanıcı Bulunamadı";
+        public static string PasswordError = "Şifre Hatalı";
+        public static string SuccessfulLogin = "Giriş Başarılı";
+        public static string UserAlreadyExists = "Bu Kullanıcı Zaten Mevcut";
+        public static string UserRegistered = "Kullanıcı Başarıyla Kaydedildi";
+        public static string AccessTokenCreated = "Token başarı ile oluşturuldu";
     }
 }
